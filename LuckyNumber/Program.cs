dc7ec471b168f415276030559cdfcc7eaa6ff2cc
Jhenny <jhennyfferemmily@hotmail.com>
2021-08-23 22:54:35 -0300

    first commit

---
+++ LuckyNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LuckyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resultado = true;
+            while (resultado)
+            {
+                int numeroAleatorio = new Random().Next(100);
+                Console.WriteLine("What's your name");
+                var name = Console.ReadLine();
+                Console.WriteLine("Your name is: " + name);
+                Console.WriteLine("Today is your lucky day!!");
+                Console.WriteLine("The Number is ");
+                Console.WriteLine(numeroAleatorio);
+
+                int number = 0;
+                while (number < 99) // condition
+                {
+                    //Console.WriteLine("number = {0}", number);
+
+                    number++;
+
+                    if (number < 99)
+                    {
+                        Console.WriteLine("You're gonna win the lottery");
+                    }
+                    else if (number > 20)
+                    {
+                        Console.WriteLine("Not lucky");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try it again next time");
+                    }
+
+                    Console.ReadLine();
+
+
+                }
+            }
+        }
+    }
+}
